e127a9e74274d7aa5a7b3cc01cb9f61d776b825f
NIHAL <nihaleloudiki16@gmail.com>
2024-04-03 18:14:49 +0000

    Python_Mysql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Create user with all privileges in localhost
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
