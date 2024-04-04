077a12f8870f36f07e501cd37fd1193f67bc7630
NIHAL <nihaleloudiki16@gmail.com>
2024-04-04 16:45:58 +0000

    Python_Mysql

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):        
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls:
             filters = {}
@@ -53,7 +53,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
@@ -63,7 +63,3 @@ class FileStorage:
         if key in FileStorage.__objects.keys():
             del FileStorage.__objects[key]
             self.save()
-            
-            
-            
-        
