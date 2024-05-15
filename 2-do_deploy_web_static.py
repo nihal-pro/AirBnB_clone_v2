3a8f823b40c991a883fcd8ca27b3d9b4bde3abe9
Nihal <nihaleloudiki16@gmail.com>
2024-05-14 17:35:03 -0700

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,51 +1,35 @@
 #!/usr/bin/python3
-"""
-import modules
-this file is fabfile
-"""
-from fabric.api import local, task, env, put, run
-from datetime import datetime
+#  Fabric script that generates a .tgz archive from the
+# contents of the web_static folder of your AirBnB Clone repo
+# using the function do_pack
 import os
+from fabric.api import run, put, env
 
 env.hosts = ['54.172.182.205', '52.91.165.220']
-env.user = 'ubuntu'
+env.user = "ubuntu"
 
 
-@task
-def do_pack():
-    """
-    this function execute script that generates a .tgz
-        archive from the contents of the web_static folder
-    """
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = f'versions/web_static_{date}.tgz'
-    print(f'Packing web_static to {file_path}')
-    try:
-        local(f"tar -czvf {file_path} web_static")
-        size = os.path.getsize(file_path)
-        print(f'web_static packed: {file_path} -> {size}Bytes')
-        return file_path
-    except Exception:
-        return None
-
-
-@task
 def do_deploy(archive_path):
-    """this function distributes an archive to your web servers"""
-    if os.path.exists(archive_path):
-        File = os.path.basename(archive_path)
-        Dir = os.path.splitext(File)[0]
-        FullPath = "/data/web_static/releases/{}".format(Dir)
-        put(archive_path, "/tmp/")
-        run("rm -rf {}".format(FullPath))
-        run("mkdir -p {}/".format(FullPath))
-        run("tar -xzf /tmp/{} -C {}/".format(File, FullPath))
-        run("rm /tmp/{}".format(File))
-        run("mv {0}/web_static/* {0}/".format(FullPath))
-        run("rm -rf {}/web_static".format(FullPath))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(FullPath))
-        print("New version deployed!")
-        return True
-    return False
+    """Create a tar gzipped archive of the directory web_static."""
+    if os.path.exists(archive_path) is False:
+        return False
+    else:
+        try:
+            put(archive_path, "/tmp/")
+            """ putting the file to .tgz """
+            file_name = archive_path.split("/")[1]
+            """ splitting .tgz """
+            file_name2 = file_name.split(".")[0]
+            """ spliting archivo """
+            final_name = "/data/web_static/releases/" + file_name2 + "/"
+            run("mkdir -p " + final_name)
+            run("tar -xzf /tmp/" + file_name + " -C " + final_name)
+            run("rm /tmp/" + file_name)
+            run("mv " + final_name + "web_static/* " + final_name)
+            run("rm -rf " + final_name + "web_static")
+            run("rm -rf /data/web_static/current")
+            run("ln -s " + final_name + " /data/web_static/current")
+            print("New version deployed!")
+            return True
+        except Exception:
+            return False
